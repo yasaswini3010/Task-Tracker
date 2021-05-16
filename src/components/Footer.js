db05668d2bf15ef58b7faccd072c20ada1926300
yasaswini3010 <yasaswini3010@gmail.com>
2021-05-16 16:43:55 +0530

    task-tracker

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+//import React from 'react'
+import {Link} from 'react-router-dom'
+import {useLocation} from 'react-router-dom'
+const Footer = () => {
+    const location=useLocation()
+    return (
+        <footer>
+            <p>Copyright &copy; 2021</p>
+            {location.pathname==='/' && <Link to='/about'>About</Link>}
+            
+        </footer>
+    )
+}
+
+export default Footer
